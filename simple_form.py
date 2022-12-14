291d87fa52c1dd927902a1293be14eef92936e4c
takamcu <takayuki.mcu@outlook.jp>
2022-12-14 14:05:10 +0900

    initial commit.

---
+++ simple_form.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("start.html")
+
+@app.route('/procForm', methods=['POST'])
+def processPost():
+    msg = request.form['msg']
+    return render_template("result.html", message=msg)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
